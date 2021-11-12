111ecd7f085e71d30a8b86fb83df4122dea45da7
RoySamson-stack <rsam9009@gmail.com>
2021-11-12 21:36:16 +0300

    relearnign react for the second time

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from "./firstpage";
+
+function App() {
+  return (
+    <div className="App">
+    <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firstpage.js
@@ -0,0 +1,20 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+function MainPage(){
+  return(
+    <div>
+      <img src="" width=""/> 
+      <h1>Funs facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Fcaebook</li>
+        <li>Powers thousands of enterprise apps, inculding mobile apps</li>
+      </ul>
+    </div>
+  )
+}
+export default MainPage;
